2a674ea80f45d6017110f2d56aa8367ac9c1a981
sugait-takumi <touch0555@gamil.com>
2019-10-22 14:55:45 +0900

    編集ファイル間違い

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '13.115.185.17', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
